341f3cf1474c53b9850e1957626c24df59f6030c
yadikaVit <yadika@mail.ru>
2021-02-07 12:50:04 +0300

    Функции объединила

--- js/main.js
+++ js/main.js
@@ -1,20 +1,14 @@
-let getRandomIntInclusive = function (min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
+const getRandomFloatInclusive = function (min, max, n) {
   if (min < 0 || max < 0 || min > max) {
     return NaN;
   }
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-getRandomIntInclusive(3, 5);
-
-const getRandomCoordinates = function (min, max, n) {
-  if (min < 0 || max < 0 || min > max) {
-    return NaN;
-  }
-  let total = Math.random() * (max - min + 1) + min;
-
+  const total = Math.random() * (max - min) + min;
   return total.toFixed(n);
-};
+}
 
-getRandomCoordinates (4, 10 ,2);
+const getRandomIntInclusive = function (min, max) {
+
+  return getRandomFloatInclusive(Math.ceil(min), Math.floor(max), 0)
+}
+
+getRandomIntInclusive (3, 5);
