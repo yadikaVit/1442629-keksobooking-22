a63ebb5d9a1f3d8996db79f447d0a0552398c6db
yadikaVit <yadika@mail.ru>
2021-03-18 12:32:00 +0300

    Ошибка с комнатами

--- js/form.js
+++ js/form.js
@@ -1,9 +1,13 @@
 export {setupForm};
 
+const MIN_TITLE_LENGTH = 30;
+const MAX_TITLE_LENGTH = 100;
+const MAX_PRICE = 1000000;
+const priceInput = document.querySelector('#price');
+
 const setupMinPrice = function (evt) {
 
   const addMinPrice = function (price) {
-    const priceInput = document.querySelector('#price');
     priceInput.placeholder = price.toString();
     priceInput.setAttribute('min', price);
   };
@@ -50,3 +54,70 @@ const setupForm = function () {
   });
 
 }
+
+
+const offerTitle = document.querySelector('#title');
+
+offerTitle.addEventListener('input', () => {
+  const valueLength = offerTitle.value.length;
+
+  if (valueLength < MIN_TITLE_LENGTH) {
+    offerTitle.setCustomValidity('Ещё ' + (MIN_TITLE_LENGTH - valueLength) +' симв.');
+  } else if (valueLength > MAX_TITLE_LENGTH) {
+    offerTitle.setCustomValidity('Удалите лишние ' + (valueLength - MAX_TITLE_LENGTH) +' симв.');
+  } else {
+    offerTitle.setCustomValidity('');
+  }
+
+  offerTitle.reportValidity();
+});
+
+priceInput.addEventListener('input', () => {
+  const valuePrice = priceInput.value;
+  if (valuePrice > MAX_PRICE) {
+    priceInput.setCustomValidity('Цена не должна превышать' + MAX_PRICE);
+  } else {
+    priceInput.setCustomValidity('');
+  }
+
+  priceInput.reportValidity();
+});
+
+
+const dependenceRoomGuest = function () {
+  const guestsNumber = document.querySelector('#capacity');
+  const roomsNumber = document.querySelector('#room_number');
+  roomsNumber.addEventListener('change', function(evt) {
+
+    const guestNumberOne = guestsNumber.querySelector('option[value=\'1\']');
+    const guestNumberTwo = guestsNumber.querySelector('option[value=\'2\']');
+    const guestNumberThree = guestsNumber.querySelector('option[value=\'3\']');
+    const guestNumberNull = guestsNumber.querySelector('option[value=\'0\']');
+
+
+    if (evt.value = 1) {
+      guestNumberOne.setAttribute("selected", "selected");
+      guestNumberTwo.setAttribute("disabled", "disabled");
+      guestNumberThree.setAttribute("disabled", "disabled");
+      guestNumberNull.setAttribute("disabled", "disabled");
+
+    } else if (evt.value = 2) {
+      guestNumberTwo.setAttribute("selected", "selected");
+      guestNumberThree.setAttribute("disabled", "disabled");
+      guestNumberNull.setAttribute("disabled", "disabled");
+
+    } else if (evt.value = 3) {
+      guestNumberThree.setAttribute("selected", "selected");
+      guestNumberNull.setAttribute("disabled", "disabled");
+
+    } else  {
+      guestNumberOne.setAttribute("disabled", "disabled");
+      guestNumberTwo.setAttribute("disabled", "disabled");
+      guestNumberThree.setAttribute("disabled", "disabled");
+      guestNumberNull.setAttribute("selected", "selected");
+    }
+
+});
+}
+
+dependenceRoomGuest();
