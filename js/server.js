95b9c4d001e622b2d89d0f617c42701c099781b9
yadikaVit <yadika@mail.ru>
2021-03-20 23:52:19 +0300

    метод POST

--- js/server.js
+++ js/server.js
@@ -7,4 +7,21 @@ fetch('https://22.javascript.pages.academy/keksobooking/data')
   .then((offers) => {
     console.log(offers);
     renderOffer(offers.slice(0, OFFERS_COUNT));
+});
+
+
+const adForm = document.querySelector('.ad-form');
+
+adForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    const formData = new FormData(evt.target);
+
+    fetch(
+      'https://22.javascript.pages.academy/keksobooking',
+      {
+        method: 'POST',
+        body: formData,
+      },
+    );
   });
