13b99618400088a69efdada18cd45d995ad9c917
yadikaVit <yadika@mail.ru>
2021-03-29 09:16:28 +0300

    Правки по защите feature

--- js/popup.js
+++ js/popup.js
@@ -34,60 +34,57 @@ const createPhotosFragment = function (photos) {
   return photosFragment;
 };
 
-const showFeatureIcons = function (features) {
-  const featureList = cardTemplate.querySelector('.popup__features');
+
+
+const getOfferFeatures = function (features, featureList) {
+
+  let featureClass = '';
 
   if (features.length === 0) {
-    featureList.classList.add('visually-hidden');
+    featureList.style.display = 'none';
+    return featureClass;
   }
-  else {
-    features.forEach((feature) => {
-      featureList.querySelector('.popup__feature--wifi').classList.add('visually-hidden');
-      featureList.querySelector('.popup__feature--dishwasher').classList.add('visually-hidden');
-      featureList.querySelector('.popup__feature--parking').classList.add('visually-hidden');
-      featureList.querySelector('.popup__feature--washer').classList.add('visually-hidden');
-      featureList.querySelector('.popup__feature--elevator').classList.add('visually-hidden');
-      featureList.querySelector('.popup__feature--conditioner').classList.add('visually-hidden');
-      switch (feature) {
-        case 'wifi':
-          featureList.querySelector('.popup__feature--wifi').classList.remove('visually-hidden');
-          break;
-        case 'dishwasher':
-          featureList.querySelector('.popup__feature--dishwasher').classList.remove('visually-hidden');
-          break;
-        case 'parking':
-          featureList.querySelector('.popup__feature--parking').classList.remove('visually-hidden');
-          break;
-        case 'washer':
-          featureList.querySelector('.popup__feature--washer').classList.remove('visually-hidden');
-          break;
-        case 'elevator':
-          featureList.querySelector('.popup__feature--elevator').classList.remove('visually-hidden');
-          break;
-        case 'conditioner':
-          featureList.querySelector('.popup__feature--conditioner').classList.remove('visually-hidden');
-          break;
-      }
-      return features;
-    });
-  }
-}
+
+  features.forEach(function(feature) {
+    switch (feature) {
+      case 'wifi':
+        featureClass += '<li class="popup__feature popup__feature--wifi"></li>';
+        break;
+      case 'dishwasher':
+        featureClass += '<li class="popup__feature popup__feature--dishwasher"></li>';
+        break;
+      case 'parking':
+        featureClass += '<li class="popup__feature popup__feature--parking"></li>';
+        break;
+      case 'washer':
+        featureClass += '<li class="popup__feature popup__feature--washer"></li>';
+        break;
+      case 'elevator':
+        featureClass += '<li class="popup__feature popup__feature--elevator"></li>';
+        break;
+      case 'conditioner':
+        featureClass += '<li class="popup__feature popup__feature--conditioner"></li>';
+        break;
+    }
+  });
+  return featureClass;
+};
 
 
 
 const renderOffer = function ({offer, author}) {
   const offersElement = cardTemplate.cloneNode(true);
-
   offersElement.querySelector('.popup__title').textContent = offer.title;
   offersElement.querySelector('.popup__text--address').textContent = offer.address;
   offersElement.querySelector('.popup__text--price').textContent = offer.price + ' ₽/ночь';
   offersElement.querySelector('.popup__type').textContent = translateType(offer.type);
   offersElement.querySelector('.popup__text--capacity').textContent = offer.rooms + ' комнаты для ' + offer.guests + ' гостей';
   offersElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + offer.checkin + ', выезд до ' + offer.checkout;
+  offersElement.querySelector('.popup__features').innerHTML = getOfferFeatures(offer.features, offersElement.querySelector('.popup__features'));
   offersElement.querySelector('.popup__description').textContent = offer.description;
   offersElement.querySelector('.popup__photos').removeChild(offersElement.querySelector('.popup__photo'));
   offersElement.querySelector('.popup__photos').appendChild(createPhotosFragment(offer.photos));
   offersElement.querySelector('.popup__avatar').src = author.avatar;
-  showFeatureIcons(offer.features);
+
   return offersElement
 }
